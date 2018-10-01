00bc9dc1a94096894b91929cb1630fa876c2fec9
Chuan <orcuslc@hotmail.com>
2018-09-30 22:11:58 -0500

    fix an error for Hi-Res screens

--- utils.py
+++ utils.py
@@ -8,11 +8,14 @@ def get_screen_resolution():
 	if os.name == 'nt':
 		user32 = ctypes.windll.user32
 		user32.SetProcessDPIAware()
-		return user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+		resolution = (user32.GetSystemMetrics(0), user32.GetSystemMetrics(1))
 	else:
 		p = subprocess.Popen('xdpyinfo', stdout = subprocess.PIPE)
 		p2 = subprocess.Popen(['awk', '/dimensions/{print $2}'], stdin = p.stdout, stdout = subprocess.PIPE)
 		p.stdout.close()
 		r, _ = p2.communicate()
 		resolution = str(r.split()[0], encoding = 'utf-8')
-		return resolution.split('x')
+		resolution = resolution.split('x')
+	if int(resolution[0]) > 1920:
+		resolution = (1920, 1080)
+	return resolution
